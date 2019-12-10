2522944f6d54889f430f983c4003a55ed03f1f95
antoinenicolleau <antoine.nicolleau@ynov.com>
2019-12-10 17:55:01 +0100

    Problème positionnement de l'image

--- game/game_init.py
+++ game/game_init.py
@@ -39,15 +39,15 @@ def things(thingx, thingy, thingw, thingh, color):
 
 
 def text_objects(text, font):
-    textSurface = font.render(text, True, black)
-    return textSurface, textSurface.get_rect()
+    text_surface = font.render(text, True, black)
+    return text_surface, text_surface.get_rect()
 
 
 def message_display(text):
-    largeText = pygame.font.Font('freesansbold.ttf', 115)
-    TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = ((display_width / 2), (display_height / 2))
-    gameDisplay.blit(TextSurf, TextRect)
+    large_text = pygame.font.Font('freesansbold.ttf', 115)
+    text_surface, text_rect = text_objects(text, large_text)
+    text_rect.center = ((display_width / 2), (display_height / 2))
+    gameDisplay.blit(text_surface, text_rect)
 
     pygame.display.update()
 
@@ -59,17 +59,50 @@ def game_intro():
 
     while intro:
         for event in pygame.event.get():
-            print(event)
+            # print(event)
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                intro = False
 
         gameDisplay.fill(white)
         large_text = pygame.font.Font('freesansbold.ttf', 115)
         text_surf, text_rect = text_objects("AUTO_CHESS", large_text)
-        text_rect.center = ((display_width / 2), (display_height / 2))
+        text_rect.center = (int(display_width / 2), int(display_height / 2))
         gameDisplay.blit(text_surf, text_rect)
         pygame.display.update()
         clock.tick(15)
 
+
+def game_start_menu():
+    intro = True
+    character = pygame.image.load('%s/images/perso.png' % os.path.dirname(__file__)).convert_alpha()
+    character = pygame.transform.scale(character, (50, 50))
+    while intro:
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                # La position de l'image ressorti est enfaite la position de l'image.
+                print('Image pos == %s' % character.get_rect())
+                print('Mouse pos == %s' % event)
+                if character.get_rect().collidepoint(x, y):
+                    print('clicked on image')
+
+        gameDisplay.fill(white)
+        large_text = pygame.font.Font('freesansbold.ttf', 115)
+        text_surf, text_rect = text_objects("AUTO_CHESS", large_text)
+        text_rect.center = (int(display_width / 2), int(display_height / 4))
+        gameDisplay.blit(text_surf, text_rect)
+        gameDisplay.blit(character, (350, 300))
+        pygame.display.update()
+        clock.tick(15)
+        pygame.display.flip()
+
+
 game_intro()
+game_start_menu()
